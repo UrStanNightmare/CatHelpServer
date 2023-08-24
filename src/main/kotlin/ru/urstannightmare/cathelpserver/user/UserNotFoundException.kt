de06fad0a255398675c19d6a9f24e165ea9f3d86
Anton Oshchepkov <antonoschepkov27@gmail.com>
2023-08-25 04:45:15 +0700

    user: [+] User.kt - entity для хранения данных о пользователе [+] UserRepository.kt - интерфейс репозитория для взаимодействия с бд на тему юзеров :) [+] UserNotFoundException.kt - исключения если юзер не найден

---
+++ src/main/kotlin/ru/urstannightmare/cathelpserver/user/UserNotFoundException.kt
@@ -0,0 +1,9 @@
+package ru.urstannightmare.cathelpserver.user
+
+class UserNotFoundException : RuntimeException {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+
+}
